d590f71719498b55524b0d950ecddd3c63ce79dd
OyukiCV <oyukivel@hotmail.com>
2019-09-13 13:57:59 -0500

    actualizando

--- pandmonium-app/src/App.js
+++ pandmonium-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           reload. hola  kakak
+           reload. hola
         </p>
         <a
           className="App-link"
