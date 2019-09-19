d99bb0c8207c79c0217bffb663f2ac8e6c2dcaca
SolGee <solsv00@hotmail.com>
2019-09-18 21:55:40 -0500

    Se imprimen las mesas sin estilos

--- pandmonium-app/src/components/table/tables.jsx
+++ pandmonium-app/src/components/table/tables.jsx
@@ -4,8 +4,6 @@ import TableList from './table';
 function AllTables(){
     return (
         <div className = "AllTables">
-          <h1>Hello CodeSandbox</h1>
-          <h2>Start editing to see some magic happen!</h2>
           <TableList
            list = {[1,2,3,4,5,6,7,8,9,10,11,12,13,14]}
            />
@@ -13,6 +11,4 @@ function AllTables(){
     )
 }
 
-
-const RootElement = document.getElementById("root");
-ReactDOM.render(<AllTables/>,RootElement);
+export default AllTables;
