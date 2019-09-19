c10337dc7230f8e29750a60cd671a334883dc907
Rosa Isela Lopez Palma <rosaiselalopezpalma@192.168.1.85>
2019-09-19 00:59:24 -0500

    creating toolbar components

--- pandmonium-app/src/index.js
+++ pandmonium-app/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import * as serviceWorker from './serviceWorker.js';
 import Login from './components/login/Login.js';
+import Toolbar from './components/toolbar/Toolbar.js';
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+// ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<Toolbar />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
