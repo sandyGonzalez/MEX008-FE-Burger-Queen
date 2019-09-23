38f162728a857878a8a0779edde545c282751505
Rosa Isela Lopez Palma <rosaiselalopezpalma@MacBook-Air-de-Rosa.local>
2019-09-23 17:22:07 -0500

    salvando cambios generales

---
+++ pandmonium-app/src/components/SelectTable/index.js
@@ -0,0 +1,14 @@
+// Dependencies
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render (){
+    return(
+       <div className="Contact">
+         <h1>elije tu mesa</h1> 
+       </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ pandmonium-app/src/components/data/menu.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    title: 'Home',
+    url: '/'
+  },
+  {
+    title: 'About Us',
+    url: '/about'
+  },
+  {
+    title: 'Contact Us',
+    url: '/contact'
+  },
+  {
+    title: 'Library',
+    url: '/library'
+  },
+  // {
+  //   title: 'Hamburguesas',
+  //   url: '/hamburgers'
+
+  // }
+
+  {
+    title: 'SelectTable',
+    url: '/selectTable'
+  }
+];
