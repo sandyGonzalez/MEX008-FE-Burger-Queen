6a64c2010a73857499802e3dee0d5b9872091dd4
OyukiCV <oyukivel@hotmail.com>
2019-09-22 23:33:08 -0500

    ctualizo archivo Burgers correcto

--- pandmonium-app/src/components/hamburgers/Burgers.jsx
+++ pandmonium-app/src/components/hamburgers/Burgers.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import './BurgerStyle.css';
-import hamburger from '../../img/hamburger.png';
+import Burger from './Burger';
 
-//con props
-function BurgerName(props){ //componente 1
-  return (
-    
-    <div className="cuadro">
-      <img className="IconBurger" alt={props.name}  src= {hamburger}/> 
-    <p className="nameHamburger"> {props.name}</p>
-    </div>
-   
-  )
+
+function AllBurgers (){
+    return (
+        <div className="container col-12"><Burger/></div>
+    )
 }
 
-function Burgers() { //componente principal
-  return (
-    <div>
-      <BurgerName name="Gula"/>
-      <BurgerName name="Malicia" />
-      <BurgerName name="Soberbia" />
-      <BurgerName name="Alma en pena" />
-      <BurgerName name="Encarnación" />
-      <BurgerName name="Banana limbo" />
-      
-    </div>
-  );
-}
-export default Burgers;
+
+export default AllBurgers; 
