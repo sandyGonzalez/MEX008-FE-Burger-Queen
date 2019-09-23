efcde9c2cd8a0a8a9105ec6d5165a969547823ba
SolGee <solsv00@hotmail.com>
2019-09-23 09:45:34 -0500

    Se corrigio ruta de hamburger.png y se agrego el archivo routes.js

--- pandmonium-app/src/index.js
+++ pandmonium-app/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import * as serviceWorker from './serviceWorker.js';
+//import { render } from 'react-dom';
+//import { BrowserRouter as Router } from 'react-router-dom';
 //import SelectTable from './views/selectTable.jsx';
 import HamburgersMenu from './views/hamburgers'
 
