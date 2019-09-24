fccbe8d3f1a25b04cab26ed4585697e48d5758b0
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-09-24 15:26:55 -0500

    Agregando el link a el boton que es la mesa en el archivo de mesas que te redirige al menu principal

--- pandmonium-app/src/components/table/table.jsx
+++ pandmonium-app/src/components/table/table.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../table/table.scss';
+import {Link} from 'react-router-dom';
 
 
 class TableList extends React.Component{
@@ -8,12 +9,14 @@ class TableList extends React.Component{
    }
    changeColor = (x)=>{
        this.setState({changeColorGreen: !this.state.changeColorGreen})
+      
    }
    render(){
        if(this.state.changeColorGreen){
            return(
-               <button style={{backgroundColor: (this.state.changeColorGreen) ? '#C4C4C4' : "#42FF00"}} onClick={this.changeColor}>
-               {this.props.number}
+               <button style={{backgroundColor: (this.state.changeColorGreen) ? '#C4C4C4' : "#42FF00"}}
+                onClick={this.changeColor}><Link to="/Menu">{this.props.number}</Link>
+               
                </button>
            )
        }
