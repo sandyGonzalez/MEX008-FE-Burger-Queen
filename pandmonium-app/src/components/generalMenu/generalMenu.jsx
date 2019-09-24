81c413d497f579616ca092b430145bf0250c8dc1
Rosa Isela Lopez Palma <rosaiselalopezpalma@MacBook-Air-de-Rosa.local>
2019-09-24 16:05:59 -0500

    Se estructura generalMenu

--- pandmonium-app/src/components/generalMenu/generalMenu.jsx
+++ pandmonium-app/src/components/generalMenu/generalMenu.jsx
@@ -1,36 +1,13 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
-import FoodMenu from './generalFood';
-
 import './menus/FoodStyle.css';
-import hamburger from './img/hamburger.png';
-import pizza from './img/pizza.png';
-import jocho from './img/jocho.png';
-import salad from './img/salad.png';
-import drink from './img/bebida.png';
-import dessert from './img/postre.png';
+import Menu from './menu';
 
 
-
-//con props
-function menuComponent(){ //componente 1
-  return (
-    <div>
-        <FoodMenu name="principal-hamburger" icon={hamburger} />
-        <FoodMenu name="principal-pizza" icon={pizza} />
-        <FoodMenu name="principal-jocho" icon={jocho} />
-        <FoodMenu name="principal-salad" icon={salad} />
-        <FoodMenu name="principal-bebida" icon={drink} />
-        <FoodMenu name="principal-postre" icon={dessert} />
-    </div>
-  )
+function AllFood (){
+    return (
+        <div className="container"><Menu/></div>
+    )
 }
 
 
-export default menuComponent;
-
- 
-  
-  
-  
-  
+export default AllFood;
