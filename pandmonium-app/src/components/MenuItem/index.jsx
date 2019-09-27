2477fff873e5a86e497956e4120dee1ebe839254
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-09-27 15:20:23 -0500

    Actualizando el boton del componente menuItem

--- pandmonium-app/src/components/MenuItem/index.jsx
+++ pandmonium-app/src/components/MenuItem/index.jsx
@@ -7,8 +7,9 @@ class MenuItem extends Component {
     render(){
         const component = this.props.item.component;
         return(
-            <Link to={{pathname: this.props.item.route, state: {component}}}><button><img className="iconMenu" src={this.props.item.icon} alt={this.props.item.id}/></button></Link>
-            <button className="cuadro"><img className="iconFood" src={this.props.item.icon} alt={this.props.item.id}/></button> 
+
+            <Link to={{pathname: this.props.item.route, state: {component}}}><button className="cuadro"><img className="iconFood" src={this.props.item.icon} alt={this.props.item.id}/></button></Link>
+           /*  <button><img className="iconMenu" src={this.props.item.icon} alt={this.props.item.id}/></button> */
         );
     }
 }
