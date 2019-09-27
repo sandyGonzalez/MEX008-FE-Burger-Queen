889b3c8f206a53c7fdd185efbc778baf80e35e9c
Rosa Isela Lopez Palma <rosaiselalopezpalma@192.168.1.4>
2019-09-26 22:26:39 -0500

    creating folder sidebar with 2 files

---
+++ pandmonium-app/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import './Style.scss';
+import HeaderToolbar from './HeaderToolbar.jsx'
+
+
+const SideDrawer = props => {
+  let drawerClasses = ['side-drawer'];
+  if(props.show){
+    drawerClasses = ['side-drawer open'];
+  }
+  return (
+    <nav className={drawerClasses.join('')}>
+       <ul>
+         <li><Link to={'/'}>Mesas</Link></li>
+         <li><Link to={'/HistorialDePago'}>Historial de Pago</Link></li>
+         <li><Link to={'/Extras'}>Extras</Link></li>
+       </ul>
+     </nav>
+    );
+};
+
+
+const Backdrop = props => (
+  <div className="backdrop" onClick = {props.click}/>
+);
+
+
+class toolbar extends Component {
+  state = {
+    sideDrawerOpen: false
+  };
+
+  drawerToggleClickHandler = () => {
+     this.setState((prevState) => {
+       return{sideDrawerOpen: !prevState.sideDrawerOpen}
+     });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({sideDrawerOpen: false});
+  };
+    render() {
+      let backdrop;
+      if(this.state.sideDrawerOpen){
+        backdrop = <Backdrop click={this.backdropClickHandler}/>;
+      }
+      return (
+        <div style={{height: '100%'}}>
+        <HeaderToolbar  drawerClickHandler={this.drawerToggleClickHandler}/>
+        <SideDrawer show={this.state.sideDrawerOpen}/>
+        {backdrop}
+        </div>  
+      )
+    }
+  }
+
+
+export default toolbar;
+
+
+
+
