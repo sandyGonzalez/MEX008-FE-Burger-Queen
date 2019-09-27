134081ff42dee18edc6fb6b2fe1f50601dda1788
Rosa Isela Lopez Palma <rosaiselalopezpalma@MacBook-Air-de-Rosa.local>
2019-09-27 13:40:14 -0500

    Comentando rutas sobreescritas

--- pandmonium-app/src/App.js
+++ pandmonium-app/src/App.js
@@ -1,19 +1,19 @@
 import React, {Component} from 'react';
 
 //Components/ Views
-import HistorialDePago from '../views/HistorialDePago.jsx';
-import Extras from '../views/Extras.jsx';
+// import HistorialDePago from '../views/HistorialDePago.jsx';
+// import Extras from '../views/Extras.jsx';
 
 //Subroutes
-import MenuComponent from '../views/MenuView';
+// import MenuComponent from '../views/MenuView';
 
 //Food
-import Hamburgers from '../views/subviews/Hamburgers';
-import Pizzas from '../views/subviews/Pizzas';
-import Hotdogs from '../views/subviews/Hotdogs';
-import Salads from '../views/subviews/Salads';
-import Drinks from '../views/subviews/Drinks';
-import Desserts from '../views/subviews/Desserts';
+// import Hamburgers from '../views/subviews/Hamburgers';
+// import Pizzas from '../views/subviews/Pizzas';
+// import Hotdogs from '../views/subviews/Hotdogs';
+// import Salads from '../views/subviews/Salads';
+// import Drinks from '../views/subviews/Drinks';
+// import Desserts from '../views/subviews/Desserts';
 
 //import Page404 from './components/Page404'
 
@@ -30,17 +30,17 @@ class App extends Component {
             <Router basename={window.location.pathname || ''}>
                 <Switch>
                     <Route exact path='/' component={Tables} />
-                    <Route exact path='/Extras' component={Extras} />
-                    <Route exact path='/HistorialDePago' component={HistorialDePago} />                                     
+                    {/* <Route exact path='/Extras' component={Extras} /> */}
+                    {/* <Route exact path='/HistorialDePago' component={HistorialDePago} />                                      */}
                     
                     
-                    <Route exact path='/Menu' component={MenuComponent} />
-                    <Route exact path='/Hamburguesas' component={Hamburgers} />
-                    <Route exact path='/Pizzas' component={Pizzas} />
-                    <Route exact path='/Hotdogs' component={Hotdogs} />
-                    <Route exact path='/Ensaladas' component={Salads} />
-                    <Route exact path='/Bebidas' component={Drinks} />
-                    <Route exact path='/Postres' component={Desserts} />  
+                    {/* <Route exact path='/Menu' component={MenuComponent} /> */}
+                    {/* <Route exact path='/Hamburguesas' component={Hamburgers} /> */}
+                    {/* <Route exact path='/Pizzas' component={Pizzas} /> */}
+                    {/* <Route exact path='/Hotdogs' component={Hotdogs} /> */}
+                    {/* <Route exact path='/Ensaladas' component={Salads} /> */}
+                    {/* <Route exact path='/Bebidas' component={Drinks} /> */}
+                    {/* <Route exact path='/Postres' component={Desserts} />   */}
                 </Switch>
             </Router>
         )
--- pandmonium-app/src/index.js
+++ pandmonium-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
+import App from './App'
 import * as serviceWorker from './serviceWorker.js';
 
-import './styles/index.css';
+// import './styles/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
