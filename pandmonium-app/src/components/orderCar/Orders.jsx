560a1ece537e78b298082aef0ddda23847ae9126
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-09-30 18:16:33 -0500

    Modificando el contenido de los componentes para el carrito

---
+++ pandmonium-app/src/components/orderCar/Orders.jsx
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+
+class Orders extends Component{
+
+    render(){
+        return(
+            <div>
+            <h4>Pedidos</h4>
+            <imput type = "text" /> //En este imput debería llamarse lo que hay en el pedido.
+            <img src="" alt="icono para desplegar"/>
+            <img src="" alt="icono de bote de basura"/>
+            </div>
+        );
+    }
+}
+
+
+export default Orders;
---
+++ pandmonium-app/src/components/orderCar/payOrder.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+
+class PayOrder extends Component{
+
+    render(){
+        return(
+            <div>
+            <h4>Pagar Orden</h4> //Debería ir un checkbox
+            <button>Boton</button> /*Boton que cambia de color */
+            </div>
+        )
+    }
+}
