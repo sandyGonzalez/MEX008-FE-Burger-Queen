4cb99775ad75fda129278c0090ba3cefe53bddd8
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-10-03 17:30:55 -0500

    Eliminando una ruta extra

--- pandmonium-app/src/views/whitRouter.jsx
+++ pandmonium-app/src/views/whitRouter.jsx
@@ -33,7 +33,6 @@ class WhitRouter extends React.Component {
                  return(<DessertView/>)
             case '/tables':
                  return(<Tables/>)
-                /*  return(<DessertView/>); */
             case '/gula':
                  return(<CustomView/>);
             // case '/sodas':
