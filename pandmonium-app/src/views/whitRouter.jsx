ca9a09163006e8035a42bee4cd638016aa4cf91d
SolGee <solsv00@hotmail.com>
2019-10-03 17:53:36 -0500

    Add/withrouter.js

--- pandmonium-app/src/views/whitRouter.jsx
+++ pandmonium-app/src/views/whitRouter.jsx
@@ -11,39 +11,36 @@ import Tables from '../views/TableSelectorView/index'
 import CustomView from '../views/CustonViews/index';
 // import SodaView from '../views/MenuItemView/sodaview';
 // import MilkshakeView from '../views/MenuItemView/milkshakeview';
-import Todos from '../firebase/todos';
-
-
 class WhitRouter extends React.Component {
-    render(){
-        console.log(this.props.location.pathname)
-        switch(this.props.location.pathname){
-            case '/':
-                return(<Login/>)
-            case '/tables':
-                return(<Tables/>)    
-            case '/Menu':
-                return(<MenuView/>);
-            case '/burgers':
-                return (<BurgerView/>);
-            case '/hotdogs':
-                 return (<HotdogView />);
-            case '/pizzas':
-                 return(<PizzaView/>);
-            case '/ensaladas':
-                 return(<SaladView/>);
-            case '/postres':
-                 return(<DessertView/>)
-             case '/soda':
-               return(<Todos />)
-            // case '/malteadas':
-            //     return(<MilkshakeView />)
-          
-            default:
-                return (<p>404</p>) 
-        }
-        
-    }
+   render(){
+       console.log(this.props.location.pathname)
+       switch(this.props.location.pathname){
+           case '/':
+               return(<Login/>)
+           case '/Menu':
+               return(<MenuView/>);
+           case '/burgers':
+               return (<BurgerView/>);
+           case '/hotdogs':
+                return (<HotdogView />);
+           case '/pizzas':
+                return(<PizzaView/>);
+           case '/ensaladas':
+                return(<SaladView/>);
+           case '/postres':
+                return(<DessertView/>)
+           case '/tables':
+                return(<Tables/>)
+                return(<DessertView/>);
+           case '/gula':
+                return(<CustomView/>);
+           // case '/sodas':
+           //     return(<SodaView />)
+           // case '/malteadas':
+           //     return(<MilkshakeView />)
+           default:
+               return (<p>404</p>)
+       }
+   }
 }
-
 export default withRouter(WhitRouter);
